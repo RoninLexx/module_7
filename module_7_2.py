b00d9e1138642447e52b4ff880c51bf6c833e260
RoninLexx <lexxpsycho@yandex.ru>
2024-10-21 23:46:49 +0700

    Add files via upload

---
+++ module_7_2.py
@@ -0,0 +1,26 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+
+    file = open(file_name, 'w', encoding='utf-8')
+    for idx, string in enumerate(strings, start=1): # start=1 начало нумерации строк в файле и на вывод
+        # не забыть - enumerate используется для получения индекса строки и самой строки из списка strings
+        byte_position = file.tell()
+        file.write(string + '\n')
+        strings_positions[(idx, byte_position)] = string  # idx - индекс строки (номер)
+
+    return strings_positions
+
+
+if __name__ == '__main__':
+    info = [
+        'Text for tell.',
+        'Используйте кодировку utf-8.',
+        'Because there are 2 languages!',
+        'Спасибо!'
+    ]
+
+    result = custom_write('test.txt', info)
+
+    # Вывод результата на консоль
+    for elem in result.items():
+        print(elem)
