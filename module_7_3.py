a582bbcc24aa0377fcd2025d19c5726a4ca94d9a
RoninLexx <lexxpsycho@yandex.ru>
2024-10-26 20:51:03 +0700

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,48 @@
+class WordsFinder:
+    def __init__(self, *files):
+        self.file_names = list(files)
+
+    def get_all_words(self):
+        all_words = {}
+        for f_n in self.file_names:
+            try:
+                with open(f_n, encoding='utf-8') as file:
+                    text = file.read().lower()  # Приводим к нижнему регистру
+                    for sign in [',', '.', '=', '!', '?', ';', ':', '-', '(', ')', '"']:
+                        text = text.replace(sign, ' ')
+                    words = text.split()
+                    all_words[f_n] = words
+            except FileNotFoundError:
+                print(f"Файл '{f_n}' не найден.")
+            except Exception as e:
+                print(f"Ошибка при обработке файла '{f_n}': {e}")
+        return all_words
+
+    def find(self, word):
+        found_words = {}
+        all_words = self.get_all_words()
+
+        word_lower = word.lower()
+        for f_n, words in all_words.items():
+            if word_lower in words:
+                found_words[f_n] = words.index(word_lower) + 1
+
+        return found_words
+
+    def count(self, word):
+        count_dict = {}
+        all_words = self.get_all_words()
+
+        word_lower = word.lower()
+        for f_n, words in all_words.items():
+            count = words.count(word_lower)
+            count_dict[f_n] = count
+
+        return count_dict
+
+
+if __name__ == '__main__':
+    finder2 = WordsFinder('test_file.txt')
+    print(finder2.get_all_words())  # Все слова
+    print(finder2.find('TEXT'))  # Индекс первого вхождения слова 'TEXT'
+    print(finder2.count('teXT'))  # Количество вхождений слова 'teXT' в тексте
